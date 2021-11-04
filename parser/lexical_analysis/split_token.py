963b208533494220c2881c726c2d2569495ca42c
nochgon <nochgon@gmail.com>
2021-11-04 14:30:34 +0900

    以前に作成したロードスクリプト解析アプリを参考用に配置
    ".\_old\auto_dataIO"以下に配置。Pythonスクリプトは"src\lib"以下にある。

--- parser/lexical_analysis/split_token.py
+++ parser/lexical_analysis/split_token.py
@@ -39,39 +39,6 @@ def split_token(scripts_rows: List[str]) -> Deque[Tuple[int, Deque[str]]]:
         if not len(script):
             deque_result.append((index_row + 1, split_to_token(script)))
 
-    """
-    # scripts_rowsを一つの文字列に統合。
-    # 改行部分には改行コードを挿入
-    script = '\n'.join(scripts_rows)
-
-    # コメントを削除
-    script = re_comment_line.sub('', script)
-    script = re_comment_block.sub('', script)
-
-    # トークン化
-    deque_result = deque()
-    for script in scripts_rows:
-        # [~]は列名のブロックになり、スプリット対象の例外扱いとなるので、
-        # まずはそこのところを分割、抽出
-        index_start = 0
-        script_raw = script
-        for re_result_split in re_element_block.finditer(script):
-            # 列ブロックより前をsplitし格納
-            script_target = script[index_start:re_result_split.start()]
-            deque_result.extend(split_by_re(script_target, re_split))
-
-            # ブロック部分はそのまま格納
-            deque_result.append(re_result_split.group())
-
-            # script, index_startを更新
-            script = script_raw[re_result_split.end()]
-            index_start = re_result_split.end()
-
-        # ブロックが無いscript or 残ったscriptをsplitし格納
-        deque_result.extend(split_by_re(script, re_split))
-        deque_result.append('\n')
-    deque_result.pop()  # 最後に追加した改行コードを除去
-    """
     return deque_result
 
 
